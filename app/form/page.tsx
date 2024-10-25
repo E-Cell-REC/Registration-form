e98836a7993e03e10415e4ae7875b82da2e42ad8
Charan <charanxmn@gmail.com>
2024-10-25 09:54:45 +0530

    Setup the base layout for the form

---
+++ app/form/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+const FormPage = () => {
+  return (
+    <div className="space-y-6 max-w-[640px] mx-auto mt-10">
+      <div>
+        <h2 className="text-2xl font-semibold my-2">Register for Eureka!</h2>
+        <p className="text-sm text-muted-foreground">
+          Fill out this form to register for Eureka, the biggest event for
+          student entrepreneurs, proudly presented by the{" "}
+          <span className="text-foreground font-medium">ECell REC.</span>
+        </p>
+        {/* Registratinon Form Component goes here */}
+      </div>
+    </div>
+  );
+};
+
+export default FormPage;
