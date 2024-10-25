5c7389340add17b1564b679ed6df99c15c34b0c7
Charan <charanxmn@gmail.com>
2024-10-25 22:11:48 +0530

    Updated the content of the form header.

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -5,11 +5,14 @@ const FormPage = () => {
   return (
     <div className="space-y-6 max-w-[640px] mx-auto mt-10">
       <div>
-        <h2 className="text-2xl font-semibold my-2">Register for Eureka!</h2>
+        <h2 className="text-2xl font-semibold my-2">
+          Register for the Illuminate Workshop!
+        </h2>
         <p className="text-sm text-muted-foreground">
-          Fill out this form to register for Eureka, the biggest event for
-          student entrepreneurs, proudly presented by the{" "}
-          <span className="text-foreground font-medium">ECell REC.</span>
+          Fill out this form to register for the{" "}
+          <span className="text-foreground">Immluminate Workshop</span>, the
+          biggest event for student entrepreneurs, proudly presented by the{" "}
+          <span className="text-foreground">ECell REC.</span>
         </p>
       </div>
       <Separator />
