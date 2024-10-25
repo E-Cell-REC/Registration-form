c791cae3bbdc7734cde58010343824c8cebdbec4
Charan <charanxmn@gmail.com>
2024-10-25 08:57:55 +0530

    Initialized a new NextJS project
    Created using npx create-next-app@14.2.5 (NextJS 15 released yesterday so it's not worth taking any chances)

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Eureka Registration",
+  description: "Register for ECell REC's biggest event for entrepresneurs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
