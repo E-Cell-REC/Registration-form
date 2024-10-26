6828e6a042f02faccdaf03e39d272916642f3d4d
Charan <charanxmn@gmail.com>
2024-10-26 17:19:19 +0530

    Added zod form schema and created a controlled form using react-hook-form

--- app/form/registration-form.tsx
+++ app/form/registration-form.tsx
@@ -1,12 +1,55 @@
 "use client";
 
-import { Form } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const registrationFormSchema = z.object({
+  fullName: z
+    .string({ required_error: "Name is required." })
+    .min(3, { message: "Name must be atleast 3 characters." }),
+  email: z.string().email({ message: "Invalid email address." }),
+  phoneNumber: z
+    .string({ required_error: "Phone number is required." })
+    .length(10, { message: "Phone number must be 10 digits." }),
+  title: z
+    .string({ required_error: "Title is required." })
+    .min(2, { message: "Name must be atleast 2 characters." }),
+  organization: z
+    .string({ required_error: "Organization is required." })
+    .min(2, { message: "Organization must be atleast 2 characters." }),
+  areaOfInterest: z.enum([
+    "Technology",
+    "Entrepreneurship",
+    "Innovation",
+    "Higher Studies",
+    "Content Creation",
+    "Management",
+  ]),
+  expectations: z.string().max(160).min(4),
+  paymentId: z.number().max(999999999999),
+});
 
 const RegistrationForm = () => {
-  const form = {};
+  const form = useForm({
+    resolver: zodResolver(registrationFormSchema),
+    defaultValues: {},
+    mode: "onChange",
+  });
   return (
-    <Form>
-      <form></form>
+    <Form {...form}>
+      <form className="space-y-8">
+      </form>
     </Form>
   );
 };
